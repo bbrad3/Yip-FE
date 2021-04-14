ecdddf861000a604757345dd46bdf8af7e1ab41d
bbrad3 <68521389+bbrad3@users.noreply.github.com>
2021-04-14 14:48:17 -0600

    Set up express app and basic file structure

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+
+const routesReport = require('rowdy-logger').begin(app)
+const path = require('path')
+const replaceInFile = require('replace-in-file')
+const morgan = require('morgan')
+
+// MIDDLEWARE
+app.use(express.json())
+app.use(morgan('dev'))
+
+// ROUTES
+
+app.get('/', (req, res) => {
+    const filepath = path.join(__dirname, './index.html')
+    res.sendFile(filepath)
+})
+    
+app.get('/main.js', (req, res) => {
+    const filepath = path.join(__dirname, './main.js')
+    res.sendFile(filepath)
+})
+    
+app.get('/style.css', (req, res) => {
+    const filepath = path.join(__dirname, './style.css')
+    res.type('css').sendFile(filepath)
+})
+
+// SERVER
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => {
+    routesReport.print()
+
+    console.log(`Server listening on port ${PORT}`)
+})
