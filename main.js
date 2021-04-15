0bf0f654d62d8ceec4c99f11f2fa0f90e4c369fd
bbrad3 <68521389+bbrad3@users.noreply.github.com>
2021-04-15 10:29:03 -0600

    User login, signup, logout working

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 console.log('Hello from main.js')
 
+// GLOBAL VARIABLES
+const backendUrl = 'http://localhost:3001'
+
 // DOM SELECTORS
 const allLinks = document.querySelectorAll('.navLink')
 const homeLink = document.querySelector('#homeLink')
@@ -22,6 +25,7 @@ const signupForm = document.querySelector('#signupForm')
 const loginForm = document.querySelector('#loginForm')
 const editBusinessForm = document.querySelector('#editBusinessForm')
 const editUserForm = document.querySelector('#editUserForm')
+
 // NAV LINKS
 homeLink.addEventListener('click', () => {
     hideViews()
@@ -35,6 +39,7 @@ signupLink.addEventListener('click', () => {
     hideViews()
     signupView.classList.remove('hidden')
 })
+logoutLink.addEventListener('click', logoutUser)
 showLink.addEventListener('click', () => {
     hideViews()
     showAllView.classList.remove('hidden')
@@ -44,8 +49,59 @@ profileLink.addEventListener('click', () => {
     profileView.classList.remove('hidden')
 })
 
-
 // USER
+// --LOGIN
+loginForm.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    try {
+        const email = document.querySelector('#loginEmail').value
+        const password = document.querySelector('#loginPassword').value
+
+        const response = await axios.post(`${backendUrl}/users/login`, {
+            email: email,
+            password: password
+        })
+        console.log('login response', response.data)
+        if(response.status === 200) {
+            localStorage.setItem('userId', response.data.user.id)
+            checkLoggedIn()
+            goHome()
+        }
+    } catch (error) {
+        console.log('Can not login user')
+    }
+})
+
+// --SIGNUP
+signupForm.addEventListener('submit', async (e) => {
+    e.preventDefault()
+    try {
+        const name = document.querySelector('#signupName').value
+        const email = document.querySelector('#signupEmail').value
+        const password = document.querySelector('#signupPassword').value
+
+        const response = await axios.post(`${backendUrl}/users/new`, {
+            name: name,
+            email: email,
+            password: password
+        })
+        console.log('signup response', response.data)
+        if(response.status === 200) {
+            localStorage.setItem('userId', response.data.user[0].id)
+            checkLoggedIn()
+            goHome()
+        }
+    } catch (error) {
+        console.log('Can not login user')
+    }
+})
+
+// --LOGOUT
+function logoutUser() {
+    localStorage.removeItem('userId')
+    checkLoggedIn()
+    goHome()
+}
 
 // BUSINESS
 
@@ -55,3 +111,23 @@ function hideViews() {
         view.classList.add('hidden')
     })
 }
+
+function goHome() {
+    hideViews()
+    homeView.classList.remove('hidden')
+}
+
+function checkLoggedIn() {
+    if(localStorage.getItem('userId')) {
+        loginLink.classList.add('hidden')
+        signupLink.classList.add('hidden')
+        logoutLink.classList.remove('hidden')
+        profileLink.classList.remove('hidden')
+    } else {
+        loginLink.classList.remove('hidden')
+        signupLink.classList.remove('hidden')
+        logoutLink.classList.add('hidden')
+        profileLink.classList.add('hidden')
+    }
+}
+checkLoggedIn()
