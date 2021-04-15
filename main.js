3a1b11eb49524f3f06ff1bf94cb68c04ae1780e8
bbrad3 <68521389+bbrad3@users.noreply.github.com>
2021-04-15 09:51:53 -0600

    Nav links show and hide views

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 console.log('Hello from main.js')
 
 // DOM SELECTORS
+const allLinks = document.querySelectorAll('.navLink')
 const homeLink = document.querySelector('#homeLink')
 const loginLink = document.querySelector('#loginLink')
 const signupLink = document.querySelector('#signupLink')
@@ -8,11 +9,12 @@ const logoutLink = document.querySelector('#logoutLink')
 const showLink = document.querySelector('#showLink')
 const profileLink = document.querySelector('#profileLink')
 
+const allViews = document.querySelectorAll('.view')
 const homeView = document.querySelector('#home')
 const signupView = document.querySelector('#signup')
 const loginView = document.querySelector('#login')
-const allBusinessesView = document.querySelector('#allBusinesses')
-const oneBusinessView = document.querySelector('#oneBusiness')
+const showAllView = document.querySelector('#allBusinesses')
+const showOneView = document.querySelector('#oneBusiness')
 const profileView = document.querySelector('#profile')
 
 const searchBar = document.querySelectorAll('.searchBar')
@@ -21,9 +23,35 @@ const loginForm = document.querySelector('#loginForm')
 const editBusinessForm = document.querySelector('#editBusinessForm')
 const editUserForm = document.querySelector('#editUserForm')
 // NAV LINKS
+homeLink.addEventListener('click', () => {
+    hideViews()
+    homeView.classList.remove('hidden')
+})
+loginLink.addEventListener('click', () => {
+    hideViews()
+    loginView.classList.remove('hidden')
+})
+signupLink.addEventListener('click', () => {
+    hideViews()
+    signupView.classList.remove('hidden')
+})
+showLink.addEventListener('click', () => {
+    hideViews()
+    showAllView.classList.remove('hidden')
+})
+profileLink.addEventListener('click', () => {
+    hideViews()
+    profileView.classList.remove('hidden')
+})
+
 
 // USER
 
 // BUSINESS
 
 // REUSABLE FUNCTIONS
+function hideViews() {
+    allViews.forEach(view => {
+        view.classList.add('hidden')
+    })
+}
